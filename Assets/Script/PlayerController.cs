4736971705ef95750f41ef05ac7faeb8fd077952
Argonaut <85242295+jeonghyeok-Lee@users.noreply.github.com>
2023-12-17 03:36:26 +0900

    feat: off파일을 xyz로 변환하는 기능 추가

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -76,6 +76,11 @@ public class PlayerController : MonoBehaviour
             FileConverter.ConvertXYZtoOFF("Assets/Resources/PointClouds/Untitled.xyz", "Assets/Resources/PointClouds/pointcloud.off");
         }
 
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            FileConverter.ConvertOFFtoXYZ("Assets/Resources/PointClouds/pointcloud.off", "Assets/Resources/PointClouds/Untitled1.xyz");
+        }
+
     }
 
     private void Move()
--- Assets/Script/Utils/FileConverter.cs
+++ Assets/Script/Utils/FileConverter.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 
 public class FileConverter : MonoBehaviour
 {
+
+    /// <summary>
+    /// xyz파일을 OFF파일로 변환하는 함수
+    /// </summary>
+    /// <param name="inputFilePath"> xyz파일을 받아오는 변수</param>
+    /// <param name="outputFilePath"> xyz파일을 off파일로 변환 후 해당 파일을 저장하기위한 위치 변수</param>
+    /// <example> ConvertXYZtoOFF("Assets/Resources/PointClouds/Untitled.xyz", "Assets/Resources/PointClouds/pointcloud.off"); </example>
     public static void ConvertXYZtoOFF(string inputFilePath, string outputFilePath)
     {
         try{
@@ -21,7 +28,28 @@ public class FileConverter : MonoBehaviour
                 }
             }
         }catch(IOException e){
-            Debug.Log(e.Message);
+            Debug.Log($"XYZ파일을 OFF로 변환하는 과정에서 오류 발생: {e.Message}");
+        }
+    }
+
+    public static void ConvertOFFtoXYZ(string inputFilePath, string outputFilePath)
+    {
+        try{
+            string[] lines = File.ReadAllLines(inputFilePath);    // OFF파일의 모든 라인을 읽어옴
+
+            using(StreamWriter outputFile = new StreamWriter(outputFilePath))
+            {
+                int numVertices = int.Parse(lines[1].Split(' ')[0]);    // 첫번째 줄은 건너뛰고 2번쨰 라인부터 읽어옴
+                
+                for(int i = 2; i < numVertices + 2; i++)                // OFF파일의 라인 수만큼 반복
+                {
+                    string[] coordinates = lines[i].Split(' ');         // OFF파일의 라인을 공백으로 나눔
+                    outputFile.WriteLine($"{coordinates[0]} {coordinates[1]} {coordinates[2]}"); // xyz파일에 씀
+                }
+            }
+        }
+        catch(IOException e){
+            Debug.Log($"OFF파일을 XYZ로 변환하는 과정에서 오류 발생: {e.Message}");
         }
     }
 }
